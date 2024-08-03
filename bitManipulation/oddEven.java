66a006d50e8735dbf8150680d8c1e8e24463ca67
gitshubhsss <shubhamranjane16@gmail.com>
2024-08-03 22:36:02 +0530

    .\oddEven.java

---
+++ bitManipulation/oddEven.java
@@ -0,0 +1,16 @@
+public class oddEven {
+    public static void main(String[] args) {
+        int n=3;
+       
+       
+
+        System.out.println();
+        
+
+        // if(n|m==n){
+        //     System.out.println("odd number");
+        // }else{
+        //     System.out.println("even number");
+        // }
+    }
+}
